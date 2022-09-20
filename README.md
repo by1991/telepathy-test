6c3df03b5df47774e9eb83aba6007d561064f162
by1991 <bduaa@connect.ust.hk>
2022-09-20 21:58:40 +0800

    implement Question1 program

--- README.md
+++ README.md
@@ -4,9 +4,18 @@
 - .Net 6 SDK or later
 
 ## Question 1
+### Run Program
+Run this command under TL.Question1 folder:
+`dotnet run`
 ### Run Unit Tests
 `dotnet test`
 
 ## Question 2
+### Run Program
+Run this command under TL.Question2 folder:
+`dotnet run`
+### Run Unit Tests
+`dotnet test`
 ### References:
 - https://www.geeksforgeeks.org/avl-tree-set-1-insertion/
+- https://en.wikipedia.org/wiki/AVL_tree
--- TL.Question1/Program.cs
+++ TL.Question1/Program.cs
@@ -19,15 +19,259 @@ foreach (string id in roomIds)
 
 IRoomService roomService = new RoomService(rooms);
 
-roomService.CheckIn();
-roomService.CheckOut("2A");
-roomService.CheckIn();
-roomService.CheckOut("1B");
-roomService.MarkRepair("1B");
-roomService.RepairRoom("1B");
-roomService.CleanRoom("1B");
-roomService.CheckIn();
-roomService.MarkRepair("2A");
-roomService.RepairRoom("2A");
-roomService.CleanRoom("2A");
-roomService.Print();
+Console.WriteLine("-----------------------------");
+Console.WriteLine("---------- Welcome ----------");
+Console.WriteLine("-----------------------------");
+Console.WriteLine();
+Console.WriteLine("Please Select An Action Below:");
+ShowMenu();
+
+void ShowMenu()
+{
+    Console.WriteLine("-- Check In, Please Type 1");
+    Console.WriteLine("-- Check Out, Please Type 2");
+    Console.WriteLine("-- Clean Room, Please Type 3");
+    Console.WriteLine("-- Mark Repair, Please Type 4");
+    Console.WriteLine("-- Repair Room, Please Type 5");
+
+    var action = Console.ReadLine();
+
+    if (action == null)
+    {
+        Console.WriteLine("Please Type A Valid Action:");
+        ShowMenu();
+    }
+    else
+    {
+        action = action.Replace(" ", "");
+    }
+
+    switch (action)
+    {
+        case "1":
+            Console.WriteLine("... Check In ...");
+            roomService.CheckIn();
+            Console.WriteLine("-- Type 1 To Continue");
+            Console.WriteLine("-- Type 2 To Exit");
+
+            var step1 = Console.ReadLine();
+
+            if (step1 != null && step1.Replace(" ", "") == "1")
+            {
+                Console.WriteLine("Please Select An Action Below:");
+                ShowMenu();
+            }
+            else if (step1 != null && step1.Replace(" ", "") == "2")
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Please Type A Valid Action:");
+                ShowMenu();
+            }
+
+            break;
+        case "2":
+            Console.WriteLine("Please provide a room number:");
+            var roomIdForCheckout = Console.ReadLine();
+            if (roomIdForCheckout != null)
+            {
+                roomService.CheckOut(roomIdForCheckout);
+                Console.WriteLine("-- Type 1 To Continue");
+                Console.WriteLine("-- Type 2 To Exit");
+
+                var step2 = Console.ReadLine();
+
+                if (step2 != null && step2.Replace(" ", "") == "1")
+                {
+                    Console.WriteLine("Please Select An Action Below:");
+                    ShowMenu();
+                }
+                else if (step2 != null && step2.Replace(" ", "") == "2")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please Type A Valid Action:");
+                    ShowMenu();
+                }
+            }
+            else
+            {
+                Console.WriteLine("-- Type 1 To Continue");
+                Console.WriteLine("-- Type 2 To Exit");
+
+                var step2 = Console.ReadLine();
+
+                if (step2 != null && step2.Replace(" ", "") == "1")
+                {
+                    Console.WriteLine("Please Select An Action Below:");
+                    ShowMenu();
+                }
+                else if (step2 != null && step2.Replace(" ", "") == "2")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please Type A Valid Action:");
+                    ShowMenu();
+                }
+            }
+
+            break;
+        case "3":
+            Console.WriteLine("Please provide a room number:");
+            var roomIdForClean = Console.ReadLine();
+            if (roomIdForClean != null)
+            {
+                roomService.CleanRoom(roomIdForClean);
+                Console.WriteLine("-- Type 1 To Continue");
+                Console.WriteLine("-- Type 2 To Exit");
+
+                var step2 = Console.ReadLine();
+
+                if (step2 != null && step2.Replace(" ", "") == "1")
+                {
+                    Console.WriteLine("Please Select An Action Below:");
+                    ShowMenu();
+                }
+                else if (step2 != null && step2.Replace(" ", "") == "2")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please Type A Valid Action:");
+                    ShowMenu();
+                }
+            }
+            else
+            {
+                Console.WriteLine("-- Type 1 To Continue");
+                Console.WriteLine("-- Type 2 To Exit");
+
+                var step2 = Console.ReadLine();
+
+                if (step2 != null && step2.Replace(" ", "") == "1")
+                {
+                    Console.WriteLine("Please Select An Action Below:");
+                    ShowMenu();
+                }
+                else if (step2 != null && step2.Replace(" ", "") == "2")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please Type A Valid Action:");
+                    ShowMenu();
+                }
+            }
+            break;
+        case "4":
+            Console.WriteLine("Please provide a room number:");
+            var roomIdForMarkRepair = Console.ReadLine();
+            if (roomIdForMarkRepair != null)
+            {
+                roomService.MarkRepair(roomIdForMarkRepair);
+                Console.WriteLine("-- Type 1 To Continue");
+                Console.WriteLine("-- Type 2 To Exit");
+
+                var step2 = Console.ReadLine();
+
+                if (step2 != null && step2.Replace(" ", "") == "1")
+                {
+                    Console.WriteLine("Please Select An Action Below:");
+                    ShowMenu();
+                }
+                else if (step2 != null && step2.Replace(" ", "") == "2")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please Type A Valid Action:");
+                    ShowMenu();
+                }
+            }
+            else
+            {
+                Console.WriteLine("-- Type 1 To Continue");
+                Console.WriteLine("-- Type 2 To Exit");
+
+                var step2 = Console.ReadLine();
+
+                if (step2 != null && step2.Replace(" ", "") == "1")
+                {
+                    Console.WriteLine("Please Select An Action Below:");
+                    ShowMenu();
+                }
+                else if (step2 != null && step2.Replace(" ", "") == "2")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please Type A Valid Action:");
+                    ShowMenu();
+                }
+            }
+            break;
+        case "5":
+            Console.WriteLine("Please provide a room number:");
+            var roomIdForRepair = Console.ReadLine();
+            if (roomIdForRepair != null)
+            {
+                roomService.RepairRoom(roomIdForRepair);
+                Console.WriteLine("-- Type 1 To Continue");
+                Console.WriteLine("-- Type 2 To Exit");
+
+                var step2 = Console.ReadLine();
+
+                if (step2 != null && step2.Replace(" ", "") == "1")
+                {
+                    Console.WriteLine("Please Select An Action Below:");
+                    ShowMenu();
+                }
+                else if (step2 != null && step2.Replace(" ", "") == "2")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please Type A Valid Action:");
+                    ShowMenu();
+                }
+            }
+            else
+            {
+                Console.WriteLine("-- Type 1 To Continue");
+                Console.WriteLine("-- Type 2 To Exit");
+
+                var step2 = Console.ReadLine();
+
+                if (step2 != null && step2.Replace(" ", "") == "1")
+                {
+                    Console.WriteLine("Please Select An Action Below:");
+                    ShowMenu();
+                }
+                else if (step2 != null && step2.Replace(" ", "") == "2")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please Type A Valid Action:");
+                    ShowMenu();
+                }
+            }
+            break;
+        default:
+            Console.WriteLine("Please Type A Valid Action:");
+            ShowMenu();
+            break;
+    }
+}
